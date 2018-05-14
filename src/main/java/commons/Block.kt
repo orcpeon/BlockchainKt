7c49c6b9a558b0bb65ec7747fa0e6f49da760148
Alexander Pukhalsky <apukhalsky@buffergroup.com>
2018-05-14 19:24:51 +0300

    Implemented mining

--- src/main/java/commons/Block.kt
+++ src/main/java/commons/Block.kt
@@ -9,7 +9,23 @@ class Block(data : String, previousHash : String) {
     var data : String = data
     var timestamp : Long = Date().time
     var hash : String = calculateHash()
+    var nonce = 0
 
-    fun calculateHash() : String = StringUtils.applySHA256(previousHash + timestamp.toString() + data)
+    fun calculateHash() : String = StringUtils.applySHA256(previousHash +
+                                                                timestamp.toString() +
+                                                                nonce.toString() +
+                                                                data)
+
+    fun mineBlock(dificulty : Int) {
+
+        val target = String(CharArray(dificulty)).replace('\u0000', '0') //Create a string with difficulty * "0"
+
+        while (!hash.substring(0, dificulty).equals(target)) {
+            nonce++
+            hash = calculateHash()
+        }
+
+        println("Block mined! Hash: $hash")
+    }
 
 }
